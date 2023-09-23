699e0a1a39889505c9a4a266407e4f925e9bb56f
Hans Walker <hbwalker@usc.edu>
2023-09-22 21:57:23 -0700

    updated models

--- Models.py
+++ Models.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 #Model to perform classification fo Mnist digits
 
-def get_classifier_image(number_of_res_blocks, regularization_const=0.01):
+def get_classifier_image(number_of_res_blocks, output_dim, regularization_const=0.01):
     #Function whcih returns a model to perform classification of Mnist digits
     input = tf.keras.Input(shape=(28,28,1))
     x = tf.keras.layers.Conv2D(filters=32, kernel_size=5, padding='same', kernel_regularizer=tf.keras.regularizers.l2(regularization_const))(input)
@@ -12,7 +12,7 @@ def get_classifier_image(number_of_res_blocks, regularization_const=0.01):
     
     x = tf.keras.layers.Conv2D(filters=1, kernel_size=5, padding='same', kernel_regularizer=tf.keras.regularizers.l2(regularization_const))(x)
     x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dense(10, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(regularization_const))(x)
+    x = tf.keras.layers.Dense(output_dim, activation='softmax', kernel_regularizer=tf.keras.regularizers.l2(regularization_const))(x)
 
     
     model = tf.keras.Model(inputs=input, outputs=x)
