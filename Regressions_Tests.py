dfc372c85ca599aa1bfaa76dd26f2549d59a444a
Hans Walker <hbwalker@usc.edu>
2023-10-12 23:28:05 -0700

    pinball loss

--- Regressions_Tests.py
+++ Regressions_Tests.py
@@ -10,19 +10,24 @@ def test_images_regression(images_train, labels_train, images_test, labels_test,
     if(previous_model):
         model = model.load_weights(model_name)
     else:
-        model.compile(optimizer='adam',
-                        loss=tf.losses.mean_squared_error())
-        model.fit(images_train, labels_train, epochs=10, batch_size=32)
 
         model_max_loss = get_regressor_image_base(32,3,4,1,regularization_const)
 
         model_max_loss.compile(optimizer='adam',
                         loss=negative_loss)
+        
         model_max_loss.fit(images_train, labels_train, epochs=10, batch_size=32)
 
         model_quantile = get_regressor_image_quantile(32,3,4,1,regularization_const)
 
+        model_quantile.compile(optimizer='adam',
+                        loss=pinball_loss_95)
+
         model_base = get_regressor_image_base(32,3,4,1,regularization_const)
+        
+        #Uses l1 loss
+        model_base.compile(optimizer='adam',
+                           loss = 'mae')
     
     max_mse = model_max_loss.predict(images_cal)
 
@@ -50,3 +55,13 @@ def test_images_regression(images_train, labels_train, images_test, labels_test,
     
 def negative_loss(y_true, y_pred):
     loss = -1*tf.losses.mean_squared_error()(y_true, y_pred)
+    
+def pinball_loss_95(y_true, y_pred):
+    tau = 0.95
+    error = y_true - y_pred
+    return tf.reduce_mean(tf.maximum(tau * error, (tau - 1) * error), axis=-1)
+
+def pinball_loss_05(y_true, y_pred):
+    tau = 0.05
+    error = y_true - y_pred
+    return tf.reduce_mean(tf.maximum(tau * error, (tau - 1) * error), axis=-1)
